b49413b696f8c280f4d1bee03b3a55ee8877568f
Szuuuken <daniel@szuuuken.at>
2012-10-10 00:29:36 +0300

    dependencies dwm-tray.sh

--- dwm-tray.sh
+++ dwm-tray.sh
@@ -1,4 +1,8 @@
 #/bin/bash
 
+#### dependencies:
+# 1) nm-applet
+# 2) stalonetray
+
 (sleep 1 && /usr/bin/ck-launch-session) &  # do this before nm-applet... I don't know why 
 /usr/bin/stalonetray --geometry 16x16 --icon-size 16 & (sleep 2 && /usr/bin/nm-applet) & 
